8e1823da3ef888034319e6d6e836676068019918
Michael <Michael@MICHAEL-PC>
2017-02-09 23:13:51 +0300

    Started cleaning

--- Fences/myCommands.cs
+++ Fences/myCommands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
@@ -14,24 +15,26 @@ namespace Fences
 {
     public class MyCommands
     {
-        //private const string vert = "pl{0}.AddVertexAt({1}, new Point2d({2}, {3})," + "{4}, {5}, {6});\r\n";
 
-        public Document AcDoc
-        {
-            get { return Application.DocumentManager.MdiActiveDocument; }
-        }
+        private Database database;
+        private Document document;
 
         [CommandMethod("CreateFence", CommandFlags.Modal)]
         public void CreateFence()
         {
-            var editor = AcDoc.Editor;
+            document = Application.DocumentManager.MdiActiveDocument;
+            database = document.Database;
+
+            var editor = document.Editor;
             var selAll = editor.GetSelection();
             var selectionSet = selAll.Value;
 
-            var xcord = new ArrayList();
-            var ycord = new ArrayList();
+            //var xcord = new ArrayList();
+            //var ycord = new ArrayList();
+            List<Point2d> points = new List<Point2d>();  
+            
 
-            using (var transaction = AcDoc.TransactionManager.StartTransaction())
+            using (var transaction = document.TransactionManager.StartTransaction())
             {
                 foreach (var id in selectionSet.GetObjectIds())
                 {
@@ -45,27 +48,22 @@ namespace Fences
                                 eWid = pl.GetEndWidthAt(j);
                             editor.WriteMessage(string.Format(vert, id, j, pt.X, pt.Y, bul, sWid, eWid));
                         */
-                        xcord.Add(pt.X);
-                        ycord.Add(pt.Y);
+                        points.Add(pt);
                     }
                 }
 
-                var segNum = new double[xcord.Count];
+                var segNum = new double[points.Count];
                 var check = new bool[segNum.Length - 1];
                 for (var j = 0; j < segNum.Length - 1; j++)
                     check[j] = false;
                 double angle;
                 int gap;
                 for (var j = 0; j < segNum.Length - 1; j++)
-                {
-                    segNum[j] =
-                        Math.Sqrt(Math.Pow((double) xcord[j + 1] - (double) xcord[j], 2) +
-                                  Math.Pow((double) ycord[j + 1] - (double) ycord[j], 2));
+                { 
+                    segNum[j] =points[j].GetDistanceTo(points[j+1]);
                     editor.WriteMessage(string.Format(segNum[j] + " "));
 
-                    var pt1 = new Point2d((double) xcord[j], (double) ycord[j]);
-                    var pt2 = new Point2d((double) xcord[j + 1], (double) ycord[j + 1]);
-                    angle = pt1.GetVectorTo(pt2).Angle + Math.PI / 2;
+                    angle = points[j].GetVectorTo(points[j+1]).Angle + Math.PI / 2;
 
                     if (j == 0 && check[j] == false)
                     {
@@ -105,16 +103,16 @@ namespace Fences
                                 double px1 = (double) xcord[j];
 
                                 double py1 = (double) ycord[j];
-                                double px2 = GetPoint(px1, py1, (double) xcord[j + 1], (double) ycord[j + 1], 100).Item1;
-                                double py2 = GetPoint(px1, py1, (double) xcord[j + 1], (double) ycord[j + 1], 100).Item2;
+                                double px2 = MoveDist(px1, py1, (double) xcord[j + 1], (double) ycord[j + 1], 100).Item1;
+                                double py2 = MoveDist(px1, py1, (double) xcord[j + 1], (double) ycord[j + 1], 100).Item2;
 
                                 for (int z = 0; z < div; z++)
                                 {
                                     editor.WriteMessage(string.Format(dd[z] + " "));
                                     Drawer(px1, py1, px2, py2, dd[z], AcDoc.Database, AcDoc, angle);
-                               //     DrawBar(GetPoint(px1, py1, px2, py2, segments + 10).Item1, GetPoint(px1, py1, px2, py2, segments + 10).Item2, AcDoc.Database, AcDoc, angle);
-                                    px1 = GetPoint(px1, py1, px2, py2, dd[z]).Item1;
-                                    py1 = GetPoint(px1, py1, px2, py2, dd[z]).Item2;
+                               //     DrawBar(MoveDist(px1, py1, px2, py2, segments + 10).Item1, MoveDist(px1, py1, px2, py2, segments + 10).Item2, AcDoc.Database, AcDoc, angle);
+                                    px1 = MoveDist(px1, py1, px2, py2, dd[z]).Item1;
+                                    py1 = MoveDist(px1, py1, px2, py2, dd[z]).Item2;
                                 }
                             }
                         }
@@ -136,20 +134,15 @@ namespace Fences
             }
         }
 
-        public static Tuple<double, double> GetPoint(double px1, double py1, double px2, double py2, double dist)
+        public Point2d MoveDist(Point2d p1, Point2d p2, double dist)
         {
-            var lmin = length(px1, py1, px2, py2);
-            var lx = pointA(px1, px2, lmin, dist);
-            var ly = pointA(py1, py2, lmin, dist);
-            return Tuple.Create(lx, ly);
+            Vector2d p12 = p1.GetVectorTo(p2);
+            return p1.Add(p12.GetNormal().MultiplyBy(dist));
         }
 
-        public static void Drawer(double px1, double py1, double px2, double py2, double dist, Database d, Document doc,
-            double ang)
+        public void Drawer(Point2d p1, Point2d p2, double dist)
         {
-            var ax = GetPoint(px1, py1, px2, py2, dist).Item1;
-            var ay = GetPoint(px1, py1, px2, py2, dist).Item2;
-            DrawBar(ax, ay, d, doc, ang);
+            DrawBar(MoveDist(p1, p2, dist), p1.GetVectorTo(p2).Angle);
         }
 
         public int Round(int i)
@@ -159,43 +152,33 @@ namespace Fences
             return i - i % 10 + 10;
         }
 
-        public static double length(double x1, double y1, double x2, double y2)
+        public void DrawBar(Point2d p, double ang)
         {
-            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-        }
-
-        public static double pointA(double x1, double x2, double l, double a)
-        {
-            var xa = a * (x2 - x1) / l + x1;
-
-            return xa;
-        }
-
-
-        public static void DrawBar(double x, double y, Database d, Document doc, double ang)
-        {
-            using (var acTrans = d.TransactionManager.StartTransaction())
+            using (var acTrans = document.TransactionManager.StartTransaction())
             {
                 BlockTable acBlkTbl;
-                acBlkTbl = acTrans.GetObject(d.BlockTableId, OpenMode.ForRead) as BlockTable;
+                acBlkTbl = acTrans.GetObject(database.BlockTableId, OpenMode.ForRead) as BlockTable;
 
                 BlockTableRecord acBlkTblRec;
                 acBlkTblRec =
                     acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
-                var xleft = x + 60;
-                var yleft = y + 90;
+                //var xleft = x + 60;
+        //        double w = 180;
+          //      double h = 120;
+                Point2d right = p.Add(new Vector2d(120, 180));
 
+                Rectangle3d rect = new Rectangle3d();
                 var bar = new Polyline();
-                bar.AddVertexAt(0, new Point2d(xleft, yleft), 0, 0, 0);
-                bar.AddVertexAt(0, new Point2d(xleft, yleft - 180), 0, 0, 0);
-                bar.AddVertexAt(0, new Point2d(xleft - 120, yleft - 180), 0, 0, 0);
-                bar.AddVertexAt(0, new Point2d(xleft - 120, yleft), 0, 0, 0);
-                bar.AddVertexAt(0, new Point2d(xleft, yleft), 0, 0, 0);
+                bar.AddVertexAt(0, right, 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(-120, 180)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(-120, -180)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(120, -180)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(120, 180)), 0, 0, 0);
 
                 bar.Closed = true;
 
-                var curUcsMatrix = doc.Editor.CurrentUserCoordinateSystem;
+                var curUcsMatrix = document.Editor.CurrentUserCoordinateSystem;
                 var curUcs = curUcsMatrix.CoordinateSystem3d;
 
                 bar.TransformBy(Matrix3d.Rotation(ang, curUcs.Zaxis, new Point3d(x, y, 0)));

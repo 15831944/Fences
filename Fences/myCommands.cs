171018bb2ca3d9847f286e1af408e7dd4f2d8542
Michael <Michael@MICHAEL-PC>
2017-02-10 01:19:08 +0300

    Made tests and code working

--- Fences/myCommands.cs
+++ Fences/myCommands.cs
@@ -43,95 +43,67 @@ namespace Fences
                     for (var j = 0; j < pl.NumberOfVertices; j++)
                     {
                         var pt = pl.GetPoint2dAt(j);
-                        /*    double bul = pl.GetBulgeAt(j),
-                                sWid = pl.GetStartWidthAt(j),
-                                eWid = pl.GetEndWidthAt(j);
-                            editor.WriteMessage(string.Format(vert, id, j, pt.X, pt.Y, bul, sWid, eWid));
-                        */
                         points.Add(pt);
+                        
+                    }
+                    for (int i = 0; i < points.Count - 1; i++)
+                    {
+                        int[] segments = Divide((int) points[i].GetDistanceTo(points[i + 1]), i, points.Count - 1);
+                        int dist = 0;
+                        for (int k = 0; k < segments.Length-1; k++)
+                        {
+                            dist += segments[k];
+                            Drawer(points[i], points[i+1], dist);
+                        }
                     }
                 }
 
-                var segNum = new double[points.Count];
-                var check = new bool[segNum.Length - 1];
-                for (var j = 0; j < segNum.Length - 1; j++)
-                    check[j] = false;
-                double angle;
-                int gap;
-                for (var j = 0; j < segNum.Length - 1; j++)
-                { 
-                    segNum[j] =points[j].GetDistanceTo(points[j+1]);
-                    editor.WriteMessage(string.Format(segNum[j] + " "));
-
-                    angle = points[j].GetVectorTo(points[j+1]).Angle + Math.PI / 2;
-
-                    if (j == 0 && check[j] == false)
-                    {
-                        Drawer((double) xcord[j], (double) ycord[j], (double) xcord[j + 1],
-                            (double) ycord[j + 1], 100, AcDoc.Database, AcDoc, angle);
-
-                        if (segNum[j] >= 430)
-                        {
-                            Drawer((double) xcord[j], (double) ycord[j], (double) xcord[j + 1],
-                                (double) ycord[j + 1], segNum[j] - 150, AcDoc.Database, AcDoc, angle);
-
-                            if (segNum[j] - 250 > 900)
-                            {
-                                int div = ((int) segNum[j] - 250) / 900; //Делим на 900 и округляем вверх
-                                if ((int) segNum[j] / 900 != 0)
-                                    div++; //div - это количество отрезков
-
-                                int[] dd = new int[div];
-                                for (int k = 0; k < div - 1; k++)
-                                {
-                                    dd[k] = ((int) segNum[j] - 250) / div;
-                                }
-                                int m = 0;
-                                for (int t = (((int)segNum[j] - 250) / div) / 10; t == 0; t--)
-                                {
-                                    m++;
-                                    if (m == div - 1)
-                                    {
-                                        m = 0;
-
-                                    }
-                                    dd[m] = dd[m] + 10;
-                                }
-
-                              //  Drawer((double) xcord[j], (double) ycord[j], (double) xcord[j + 1],
-                           //         (double) ycord[j + 1], 100, AcDoc.Database, AcDoc, angle);
-                                double px1 = (double) xcord[j];
-
-                                double py1 = (double) ycord[j];
-                                double px2 = MoveDist(px1, py1, (double) xcord[j + 1], (double) ycord[j + 1], 100).Item1;
-                                double py2 = MoveDist(px1, py1, (double) xcord[j + 1], (double) ycord[j + 1], 100).Item2;
-
-                                for (int z = 0; z < div; z++)
-                                {
-                                    editor.WriteMessage(string.Format(dd[z] + " "));
-                                    Drawer(px1, py1, px2, py2, dd[z], AcDoc.Database, AcDoc, angle);
-                               //     DrawBar(MoveDist(px1, py1, px2, py2, segments + 10).Item1, MoveDist(px1, py1, px2, py2, segments + 10).Item2, AcDoc.Database, AcDoc, angle);
-                                    px1 = MoveDist(px1, py1, px2, py2, dd[z]).Item1;
-                                    py1 = MoveDist(px1, py1, px2, py2, dd[z]).Item2;
-                                }
-                            }
-                        }
-
-                        check[j] = true;
-                    }
-                    else
-                    {
-                        if (j == segNum.Length - 2 && segNum[j] < 430 && check[j] == false)
-                        {
-                            Drawer((double) xcord[j], (double) ycord[j], (double) xcord[j + 1],
-                                (double) ycord[j + 1], segNum[segNum.Length - 2] - 100, AcDoc.Database, AcDoc, angle);
-                            check[j] = true;
-                        }
-                    }
-               }
 
                 transaction.Commit();
             }
+        }
+
+        public static int[] Divide(int lenght, int index, int n)
+        {
+            if (lenght < 200)
+            {
+                throw new ArgumentException("Такой длины не бывает: " + lenght);
+            }
+            int firstLen = 150;
+            int lastLen = 150;
+
+            if (index == 0)
+            {
+                firstLen = 100;
+            }
+            if (index == n - 1)
+            {
+                lastLen = 100;
+            }
+
+            if (lenght <firstLen+190+lastLen)
+            {
+                if (index == 0)
+                {
+                    return new int[] { firstLen, lenght - firstLen };
+                }
+                return new int[] {lenght - lastLen, lastLen};
+            }
+                int middleLen = lenght - firstLen - lastLen;
+            int numSeg = middleLen % 900 == 0 ? middleLen / 900 : middleLen/900 +1;
+            int minSegLenght = ((middleLen / numSeg) / 10) * 10;
+            int rest = middleLen - numSeg * minSegLenght;
+            int[] result = new int[numSeg + 2];
+            result[0] = firstLen;
+            result[result.Length - 1] = lastLen;
+            for (int i = 1; i < result.Length - 1; i++)
+            {
+                result[i] = minSegLenght;
+                int curRest = Math.Min(rest, 10);
+                result[i] += curRest;
+                rest -= curRest;
+            }
+            return result;
         }
 
         public Point2d MoveDist(Point2d p1, Point2d p2, double dist)
@@ -164,24 +136,25 @@ namespace Fences
                     acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
                 //var xleft = x + 60;
-        //        double w = 180;
-          //      double h = 120;
-                Point2d right = p.Add(new Vector2d(120, 180));
+                double w = 180;
+               double h = 120;
+              //  Point2d right = p.Add(new Vector2d(120, 180));
 
-                Rectangle3d rect = new Rectangle3d();
+             //   Rectangle3d rect = new Rectangle3d();
                 var bar = new Polyline();
-                bar.AddVertexAt(0, right, 0, 0, 0);
-                bar.AddVertexAt(0, p.Add(new Vector2d(-120, 180)), 0, 0, 0);
-                bar.AddVertexAt(0, p.Add(new Vector2d(-120, -180)), 0, 0, 0);
-                bar.AddVertexAt(0, p.Add(new Vector2d(120, -180)), 0, 0, 0);
-                bar.AddVertexAt(0, p.Add(new Vector2d(120, 180)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(w/2, h/2)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(-w/2, h/2)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(-w/2, -h/2)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(w/2, -h/2)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(w / 2, h / 2)), 0, 0, 0);
 
                 bar.Closed = true;
+
 
                 var curUcsMatrix = document.Editor.CurrentUserCoordinateSystem;
                 var curUcs = curUcsMatrix.CoordinateSystem3d;
 
-                bar.TransformBy(Matrix3d.Rotation(ang, curUcs.Zaxis, new Point3d(x, y, 0)));
+                bar.TransformBy(Matrix3d.Rotation(ang, curUcs.Zaxis, new Point3d(p.X, p.Y, 0)));
 
                 acBlkTblRec.AppendEntity(bar);
                 acTrans.AddNewlyCreatedDBObject(bar, true);
---
+++ FencesTests/MyCommandsTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fences;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fences.Tests
+{
+    [TestClass()]
+    public class MyCommandsTests
+    {
+        
+
+        [TestMethod()]
+        public void LengthOfAllSegmentsMustBeDivisibleByTen()
+        {
+            int[] segments = MyCommands.Divide(5000, 0, 3);
+            Assert.AreEqual(8,segments.Length);
+            Assert.AreEqual(100, segments[0]);
+            Assert.AreEqual(800, segments[1]);
+            Assert.AreEqual(790, segments[2]);
+            Assert.AreEqual(790, segments[3]);
+            Assert.AreEqual(790, segments[4]);
+            Assert.AreEqual(790, segments[5]);
+            Assert.AreEqual(790, segments[6]);
+            Assert.AreEqual(150, segments[7]);
+            
+        }
+
+        [TestMethod()]
+        public void Test()
+        {
+            int[] segments = MyCommands.Divide(5000, 0, 1);
+            Assert.AreEqual(8, segments.Length);
+            Assert.AreEqual(100, segments[0]);
+            Assert.AreEqual(800, segments[1]);
+            Assert.AreEqual(800, segments[2]);
+            Assert.AreEqual(800, segments[3]);
+            Assert.AreEqual(800, segments[4]);
+            Assert.AreEqual(800, segments[5]);
+            Assert.AreEqual(800, segments[6]);
+            Assert.AreEqual(100, segments[7]);
+
+        }
+
+        [TestMethod()]
+        public void DivisionWithoutRest()
+        {
+            int[] segments = MyCommands.Divide(2100, 2, 5);
+            Assert.AreEqual(4, segments.Length);
+            Assert.AreEqual(150, segments[0]);
+            Assert.AreEqual(900, segments[1]);
+            Assert.AreEqual(900, segments[2]);
+            Assert.AreEqual(150, segments[3]);
+
+        }
+
+        [TestMethod()]
+        public void IfLineIsTooSmallThereMustBeOneBar()
+        {
+            int[] segments = MyCommands.Divide(200, 0, 3);
+            Assert.AreEqual(2, segments.Length);
+            Assert.AreEqual(100, segments[0]);
+            Assert.AreEqual(100, segments[1]);
+
+        }
+
+        [TestMethod()]
+        public void IfLastOneIsTooSmallThereMustBeOneBar()
+        {
+            int[] segments = MyCommands.Divide(230, 2, 3);
+            Assert.AreEqual(2, segments.Length);
+            Assert.AreEqual(130, segments[0]);
+            Assert.AreEqual(100, segments[1]);
+
+        }
+
+    }
+}

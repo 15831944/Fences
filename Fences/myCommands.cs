967baf3abe5ad749ab966ab56f9e31d3dcc2ee91
Michael <Michael@MICHAEL-PC>
2017-02-10 11:33:16 +0300

    Cleaned a bit

--- Fences/myCommands.cs
+++ Fences/myCommands.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using Fences;
@@ -15,45 +15,41 @@ namespace Fences
 {
     public class MyCommands
     {
-
-        private Database database;
-        private Document document;
+        private Database _database;
+        private Document _document;
 
         [CommandMethod("CreateFence", CommandFlags.Modal)]
         public void CreateFence()
         {
-            document = Application.DocumentManager.MdiActiveDocument;
-            database = document.Database;
+            _document = Application.DocumentManager.MdiActiveDocument;
+            _database = _document.Database;
 
-            var editor = document.Editor;
-            var selAll = editor.GetSelection();
-            var selectionSet = selAll.Value;
+            Editor editor = _document.Editor;
+            PromptSelectionResult selAll = editor.GetSelection();
+            SelectionSet selectionSet = selAll.Value;
 
-            //var xcord = new ArrayList();
-            //var ycord = new ArrayList();
-            List<Point2d> points = new List<Point2d>();  
-            
+            List<Point2d> points = new List<Point2d>();
 
-            using (var transaction = document.TransactionManager.StartTransaction())
+
+            using (Transaction transaction = _document.TransactionManager.StartTransaction())
             {
-                foreach (var id in selectionSet.GetObjectIds())
+                foreach (ObjectId id in selectionSet.GetObjectIds())
                 {
-                    var pl = (Polyline) transaction.GetObject(id, OpenMode.ForRead);
+                    Polyline pl = (Polyline) transaction.GetObject(id, OpenMode.ForRead);
 
-                    for (var j = 0; j < pl.NumberOfVertices; j++)
+                    for (int j = 0; j < pl.NumberOfVertices; j++)
                     {
-                        var pt = pl.GetPoint2dAt(j);
+                        Point2d pt = pl.GetPoint2dAt(j);
                         points.Add(pt);
-                        
                     }
                     for (int i = 0; i < points.Count - 1; i++)
                     {
                         int[] segments = Divide((int) points[i].GetDistanceTo(points[i + 1]), i, points.Count - 1);
                         int dist = 0;
-                        for (int k = 0; k < segments.Length-1; k++)
+                        for (int k = 0; k < segments.Length - 1; k++)
                         {
                             dist += segments[k];
-                            Drawer(points[i], points[i+1], dist);
+                            Drawer(points[i], points[i + 1], dist);
                         }
                     }
                 }
@@ -66,32 +62,24 @@ namespace Fences
         public static int[] Divide(int lenght, int index, int n)
         {
             if (lenght < 200)
-            {
                 throw new ArgumentException("Такой длины не бывает: " + lenght);
-            }
             int firstLen = 150;
             int lastLen = 150;
 
             if (index == 0)
-            {
                 firstLen = 100;
-            }
             if (index == n - 1)
-            {
                 lastLen = 100;
-            }
 
-            if (lenght <firstLen+190+lastLen)
+            if (lenght < firstLen + 190 + lastLen)
             {
                 if (index == 0)
-                {
-                    return new int[] { firstLen, lenght - firstLen };
-                }
-                return new int[] {lenght - lastLen, lastLen};
+                    return new[] {firstLen, lenght - firstLen};
+                return new[] {lenght - lastLen, lastLen};
             }
-                int middleLen = lenght - firstLen - lastLen;
-            int numSeg = middleLen % 900 == 0 ? middleLen / 900 : middleLen/900 +1;
-            int minSegLenght = ((middleLen / numSeg) / 10) * 10;
+            int middleLen = lenght - firstLen - lastLen;
+            int numSeg = middleLen % 900 == 0 ? middleLen / 900 : middleLen / 900 + 1;
+            int minSegLenght = middleLen / numSeg / 10 * 10;
             int rest = middleLen - numSeg * minSegLenght;
             int[] result = new int[numSeg + 2];
             result[0] = firstLen;
@@ -126,33 +114,31 @@ namespace Fences
 
         public void DrawBar(Point2d p, double ang)
         {
-            using (var acTrans = document.TransactionManager.StartTransaction())
+            using (Transaction acTrans = _document.TransactionManager.StartTransaction())
             {
                 BlockTable acBlkTbl;
-                acBlkTbl = acTrans.GetObject(database.BlockTableId, OpenMode.ForRead) as BlockTable;
+                acBlkTbl = acTrans.GetObject(_database.BlockTableId, OpenMode.ForRead) as BlockTable;
 
                 BlockTableRecord acBlkTblRec;
                 acBlkTblRec =
                     acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
-                //var xleft = x + 60;
-                double w = 180;
-               double h = 120;
-              //  Point2d right = p.Add(new Vector2d(120, 180));
 
-             //   Rectangle3d rect = new Rectangle3d();
-                var bar = new Polyline();
-                bar.AddVertexAt(0, p.Add(new Vector2d(w/2, h/2)), 0, 0, 0);
-                bar.AddVertexAt(0, p.Add(new Vector2d(-w/2, h/2)), 0, 0, 0);
-                bar.AddVertexAt(0, p.Add(new Vector2d(-w/2, -h/2)), 0, 0, 0);
-                bar.AddVertexAt(0, p.Add(new Vector2d(w/2, -h/2)), 0, 0, 0);
+                double w = 180;
+                double h = 120;
+
+                Polyline bar = new Polyline();
+                bar.AddVertexAt(0, p.Add(new Vector2d(w / 2, h / 2)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(-w / 2, h / 2)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(-w / 2, -h / 2)), 0, 0, 0);
+                bar.AddVertexAt(0, p.Add(new Vector2d(w / 2, -h / 2)), 0, 0, 0);
                 bar.AddVertexAt(0, p.Add(new Vector2d(w / 2, h / 2)), 0, 0, 0);
 
                 bar.Closed = true;
 
 
-                var curUcsMatrix = document.Editor.CurrentUserCoordinateSystem;
-                var curUcs = curUcsMatrix.CoordinateSystem3d;
+                Matrix3d curUcsMatrix = _document.Editor.CurrentUserCoordinateSystem;
+                CoordinateSystem3d curUcs = curUcsMatrix.CoordinateSystem3d;
 
                 bar.TransformBy(Matrix3d.Rotation(ang, curUcs.Zaxis, new Point3d(p.X, p.Y, 0)));
 

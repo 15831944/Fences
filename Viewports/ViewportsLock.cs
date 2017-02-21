175fd81565cebab2b1f5cb27d32c9fdb670e6ffc
Michael <Michael@MICHAEL-PC>
2017-02-21 17:15:40 +0300

    Added new project for viewports lock

---
+++ Viewports/ViewportsLock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+using Viewports;
+
+[assembly: CommandClass(typeof(ViewportsLock))]
+
+namespace Viewports
+{
+    public class ViewportsLock
+    {
+        [CommandMethod("MyGroup", "MyCommand", "MyCommandLocal", CommandFlags.Modal)]
+        public void MyCommand() // This method can have any name
+        {
+            // Put your command code here
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed;
+            if (doc != null)
+            {
+                ed = doc.Editor;
+                ed.WriteMessage("Hello, this is your first command.");
+
+            }
+        }
+    }
+
+}

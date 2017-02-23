92fbdb27739c0db873718472340ab72ee07053a3
Michael <Michael@MICHAEL-PC>
2017-02-23 22:17:26 +0300

    Made VpLock work correctly

--- Viewports/ViewportsLock.cs
+++ Viewports/ViewportsLock.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using Autodesk.AutoCAD.Runtime;
-using Autodesk.AutoCAD.ApplicationServices;
+﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
 using Viewports;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
 
 [assembly: CommandClass(typeof(ViewportsLock))]
 
@@ -12,19 +11,52 @@ namespace Viewports
 {
     public class ViewportsLock
     {
-        [CommandMethod("MyGroup", "MyCommand", "MyCommandLocal", CommandFlags.Modal)]
-        public void MyCommand() // This method can have any name
+        [CommandMethod("LockViewport", CommandFlags.Modal)]
+        public void LockViewport()
         {
-            // Put your command code here
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Editor ed;
-            if (doc != null)
-            {
-                ed = doc.Editor;
-                ed.WriteMessage("Hello, this is your first command.");
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
 
+            using (Transaction transaction = acCurDb.TransactionManager.StartTransaction())
+            {
+                PromptSelectionResult prompt = acDoc.Editor.SelectAll();
+
+                if (prompt.Status != PromptStatus.OK) return;
+                SelectionSet selectionSet = prompt.Value;
+
+                foreach (SelectedObject obj in selectionSet)
+                    if (obj.ObjectId.ObjectClass == RXObject.GetClass(typeof(Viewport)))
+                    {
+                        Viewport viewport = (Viewport) transaction.GetObject(obj.ObjectId, OpenMode.ForWrite);
+                        viewport.Locked = true;
+                    }
+
+                transaction.Commit();
+            }
+        }
+
+        [CommandMethod("UnlockViewport", CommandFlags.Modal)]
+        public void UnlockViewport()
+        {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            using (Transaction transaction = acCurDb.TransactionManager.StartTransaction())
+            {
+                PromptSelectionResult prompt = acDoc.Editor.SelectAll();
+
+                if (prompt.Status != PromptStatus.OK) return;
+                SelectionSet selectionSet = prompt.Value;
+
+                foreach (SelectedObject obj in selectionSet)
+                    if (obj.ObjectId.ObjectClass == RXObject.GetClass(typeof(Viewport)))
+                    {
+                        Viewport viewport = (Viewport)transaction.GetObject(obj.ObjectId, OpenMode.ForWrite);
+                        viewport.Locked = false;
+                    }
+
+                transaction.Commit();
             }
         }
     }
-
 }

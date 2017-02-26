1b599cd1e94791453fd6f05197b9bff6b1445205
Home PC <kramihm@home>
2017-02-26 22:57:06 +0300

    Added NOD

--- Fences/SimpleFences.cs
+++ Fences/SimpleFences.cs
@@ -52,6 +52,98 @@ namespace Fences
             MySelect();
         }
 
+        [CommandMethod("GetFromDB", CommandFlags.Modal)]
+        public void GetFromDB()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+
+            PromptEntityResult ers = ed.GetEntity("Select entity to add" +
+                                                   " extension dictionary ");
+            if (ers.Status != PromptStatus.OK)
+                return;
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                DBObject dbObj = tr.GetObject(ers.ObjectId,
+                    OpenMode.ForRead);
+
+                ObjectId extId = dbObj.ExtensionDictionary;
+
+                if (extId == ObjectId.Null)
+                {
+                    ed.WriteMessage("У этого объекта нет сохраненных данных");
+                }
+
+                DBDictionary dbExt =(DBDictionary)tr.GetObject(extId, OpenMode.ForRead);
+                ObjectId recID = dbExt.GetAt("TEST");
+
+                Xrecord readBack = (Xrecord)tr.GetObject(
+                                    recID, OpenMode.ForRead);
+                foreach (TypedValue value in readBack.Data)
+                    System.Diagnostics.Debug.Print("===== OUR DATA: " + value.TypeCode.ToString()+ ". " + value.Value.ToString());
+
+            }
+
+        }
+
+        [CommandMethod("SaveToDB", CommandFlags.Modal)]
+        public void SaveToDB()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+
+            PromptEntityResult ers = ed.GetEntity("Select entity to add" +
+                                                   " extension dictionary ");
+            if (ers.Status != PromptStatus.OK)
+                return;
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                DBObject dbObj = tr.GetObject(ers.ObjectId,
+                                                           OpenMode.ForRead);
+
+                ObjectId extId = dbObj.ExtensionDictionary;
+
+                if (extId == ObjectId.Null)
+                {
+                    dbObj.UpgradeOpen();
+                    dbObj.CreateExtensionDictionary();
+                    extId = dbObj.ExtensionDictionary;
+                }
+
+                //now we will have extId...
+                DBDictionary dbExt =
+                        (DBDictionary)tr.GetObject(extId, OpenMode.ForRead);
+
+                //if not present add the data
+                if (!dbExt.Contains("TEST"))
+                {
+                    dbExt.UpgradeOpen();
+                    Xrecord xRec = new Xrecord();
+                    ResultBuffer rb = new ResultBuffer();
+                    rb.Add(new TypedValue(
+                              (int)DxfCode.ExtendedDataAsciiString, "Data"));
+                    rb.Add(new TypedValue((int)DxfCode.ExtendedDataReal, new double[] {1,2,3}));
+
+                    //set the data
+                    xRec.Data = rb;
+
+                    dbExt.SetAt("TEST", xRec);
+                    tr.AddNewlyCreatedDBObject(xRec, true);
+                }
+                else
+                {
+                    ed.WriteMessage("entity contains the TEST data\n");
+                }
+
+
+                tr.Commit();
+            }
+        }
+
         [CommandMethod("CreateFenceGet", CommandFlags.Modal)]
         public void CreateFenceGet()
         {

a4fd9d1b87866ab231988f5a94bf3527c9d499b1
Michael <Michael@MICHAEL-PC>
2017-03-03 14:18:11 +0300

    Moved  UserSelection to different class and minor clean

--- FencesTests/MyCommandsTests.cs
+++ FencesTests/MyCommandsTests.cs
@@ -6,10 +6,11 @@ namespace FencesTests
     [TestClass]
     public class MyCommandsTests
     {
+        readonly UserSelection _userSelection = new UserSelection();
         [TestMethod]
         public void LengthOfAllSegmentsMustBeDivisibleByTen()
         {
-            int[] segments = SimpleFences.Divide(5000, 0, 3);
+            int[] segments = _userSelection.Divide(5000, 0, 3);
             Assert.AreEqual(8, segments.Length);
             Assert.AreEqual(100, segments[0]);
             Assert.AreEqual(800, segments[1]);
@@ -24,7 +25,7 @@ namespace FencesTests
         [TestMethod]
         public void Test()
         {
-            int[] segments = SimpleFences.Divide(5000, 0, 1);
+            int[] segments = _userSelection.Divide(5000, 0, 1);
             Assert.AreEqual(8, segments.Length);
             Assert.AreEqual(100, segments[0]);
             Assert.AreEqual(800, segments[1]);
@@ -39,7 +40,7 @@ namespace FencesTests
         [TestMethod]
         public void DivisionWithoutRest()
         {
-            int[] segments = SimpleFences.Divide(2100, 2, 5);
+            int[] segments = _userSelection.Divide(2100, 2, 5);
             Assert.AreEqual(4, segments.Length);
             Assert.AreEqual(150, segments[0]);
             Assert.AreEqual(900, segments[1]);
@@ -50,7 +51,7 @@ namespace FencesTests
         [TestMethod]
         public void IfLineIsTooSmallThereMustBeOneBar()
         {
-            int[] segments = SimpleFences.Divide(200, 0, 3);
+            int[] segments = _userSelection.Divide(200, 0, 3);
             Assert.AreEqual(2, segments.Length);
             Assert.AreEqual(100, segments[0]);
             Assert.AreEqual(100, segments[1]);
@@ -59,7 +60,7 @@ namespace FencesTests
         [TestMethod]
         public void IfLastOneIsTooSmallThereMustBeOneBar()
         {
-            int[] segments = SimpleFences.Divide(230, 2, 3);
+            int[] segments = _userSelection.Divide(230, 2, 3);
             Assert.AreEqual(2, segments.Length);
             Assert.AreEqual(130, segments[0]);
             Assert.AreEqual(100, segments[1]);
